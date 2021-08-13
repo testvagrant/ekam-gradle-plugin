64a40f6d5bd5c5a3675f655a7c002f09a9046f6e
krishnb <kbandame@gmail.com>
2021-08-13 17:38:17 +0530

    Added runMobileTests and runDBTests task

--- src/main/groovy/com/testvagrant/ekam/gradle/plugins/EkamGradlePlugin.groovy
+++ src/main/groovy/com/testvagrant/ekam/gradle/plugins/EkamGradlePlugin.groovy
@@ -1,6 +1,8 @@
 package com.testvagrant.ekam.gradle.plugins
 
 import com.testvagrant.ekam.gradle.tasks.ApiTask
+import com.testvagrant.ekam.gradle.tasks.DBTask
+import com.testvagrant.ekam.gradle.tasks.MobileTask
 import com.testvagrant.ekam.gradle.tasks.WebTask
 import org.gradle.api.Plugin
 import org.gradle.api.Project
@@ -11,5 +13,7 @@ class EkamGradlePlugin implements Plugin<Project> {
     void apply(Project project) {
         project.getTasks().create("runWebTests", WebTask.class).setGroup(group);
         project.getTasks().create("runApiTests", ApiTask.class).setGroup(group);
+        project.getTasks().create("runMobileTests", MobileTask.class).setGroup(group);
+        project.getTasks().create("runDBTests", DBTask.class).setGroup(group);
     }
 }
